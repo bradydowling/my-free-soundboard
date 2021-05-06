969d00648c3fc0d721549e03a731bcc2482eba8d
bradydowling <bradydowling@gmail.com>
2021-05-05 20:51:31 -0400

    Add recorder

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ReactMic } from 'react-mic';
 
 function App() {
+  const [isRecording, setIsRecording] = useState(false);
+
+  const startRecording = () => {
+    setIsRecording(true);
+  };
+
+  const stopRecording = () => {
+    setIsRecording(false);
+  };
+
+  const onStop = (recordedBlob: any) => {
+    console.log("finished recording:", recordedBlob);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +25,14 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <ReactMic
+          record={isRecording}
+          className="sound-wave"
+          onStop={onStop}
+          strokeColor="#000000"
+          backgroundColor="#FF4081" />
+          <button onClick={startRecording} type="button">Start</button>
+        <button onClick={stopRecording} type="button">Stop</button>
         <a
           className="App-link"
           href="https://reactjs.org"
